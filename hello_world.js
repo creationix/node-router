313b036af6febb777e14d8a6efb40cb4490fd3b4
Tim Caswell <tim@creationix.com>
2010-03-26 17:49:09 -0500

    Allow simple sync responses for html (strings) and json (objects).

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,12 @@
 var server = require('./lib/node-router').getServer();
 
-server.get(new RegExp("^/(.*)$"), function hello(req, res, match) {
-  res.simpleHtml(200, "Hello " + (match || "World") + "!");
+server.get("/json", function (req, res, match) {
+  return {hello: "World"};
 });
+
+server.get(new RegExp("^/(.*)$"), function hello(req, res, match) {
+  return "Hello " + (match || "World") + "!";
+});
+
 
 server.listen(8080);
